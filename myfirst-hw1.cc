5ff6d9b19b23a1f5361aab427081e74a1c5a1866
A-RAK-518 <ajaykumar.ravipati@sjsu.edu>
2020-02-03 23:23:35 -0800

    fourth commit

--- myfirst-hw1.cc
+++ myfirst-hw1.cc
@@ -38,8 +38,8 @@ main (int argc, char *argv[])
   nodes.Create (2);
 
   PointToPointHelper pointToPoint;
-  pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
-  pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
+/*  pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
+  pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms")); */
 
   NetDeviceContainer devices;
   devices = pointToPoint.Install (nodes);
@@ -56,16 +56,16 @@ main (int argc, char *argv[])
 
   ApplicationContainer serverApps = echoServer.Install (nodes.Get (1));
   serverApps.Start (Seconds (1.0));
-  serverApps.Stop (Seconds (10.0));
+  serverApps.Stop (Seconds (35.0));
 
   UdpEchoClientHelper echoClient (interfaces.GetAddress (1), 9);
   echoClient.SetAttribute ("MaxPackets", UintegerValue (1));
   echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
-  echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
+/*  echoClient.SetAttribute ("PacketSize", UintegerValue (1024)); */
 
   ApplicationContainer clientApps = echoClient.Install (nodes.Get (0));
   clientApps.Start (Seconds (2.0));
-  clientApps.Stop (Seconds (10.0));
+  clientApps.Stop (Seconds (35.0));
 
   Simulator::Run ();
   Simulator::Destroy ();
